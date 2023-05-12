a9b80058d6ae8590ff7214f61443941e9f585669
coleow <coleow@oregonstate.edu>
2023-05-11 21:04:23 -0700

    stuff from project

---
+++ microservice.c
@@ -0,0 +1,38 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <Windows.h>
+
+
+
+int main(){
+    printf("Microservice running:\n");
+    char test[] = "go";
+    while(1){
+        Sleep(1000);
+        FILE* readfile = fopen("starterfile.txt", "r");
+        if(readfile == NULL){
+            printf("File didn't open correctly. Terminating.");
+            return 1;
+        }
+
+        char isGo[10];
+        fscanf(readfile, "%s", isGo);
+        int result = strcmp(test, isGo);
+        
+        if(result == 0){
+            fclose((fopen("starterfile.txt", "w")));
+            FILE* writefile = fopen("transferfile.txt", "w");
+            printf("Please enter the token of the stock that you would like to see: ");
+            char token[10];
+            scanf("%s", &token);
+            fprintf(writefile, "%s", token);
+            fclose(writefile);
+            printf("Resetting...");
+            result = 1;
+            strcpy(isGo, "");
+        }
+        fclose(readfile);
+    }
+    return 0;
+}
